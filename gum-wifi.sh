5c2460291d85eb1292199d8a73a574ec7dabbf7e
emancipat3r <23742165+emancipat3r@users.noreply.github.com>
2026-04-27 07:26:56 -0400

    fix(install): import GPG key for Fedora gum installation

--- gum-wifi.sh
+++ gum-wifi.sh
@@ -122,7 +122,8 @@ name=Charm
 baseurl=https://repo.charm.sh/yum/
 enabled=1
 gpgcheck=1
-gpgkey=https://repo.charm.sh/yum/gpg.key' | sudo tee /etc/yum.repos.d/charm.repo
+gpgkey=https://repo.charm.sh/yum/gpg.key' | sudo tee /etc/yum.repos.d/charm.repo >/dev/null
+				sudo rpm --import https://repo.charm.sh/yum/gpg.key
 				sudo $PM install gum -y
 				return
 				;;
