8834c8930279b2146790bc2f1ad585a652b4c9a5
emancipat3r <23742165+emancipat3r@users.noreply.github.com>
2026-04-27 07:26:27 -0400

    fix(install): correctly register Charm apt repo and key for Debian/Ubuntu

--- gum-wifi.sh
+++ gum-wifi.sh
@@ -111,7 +111,8 @@ install_package() {
 			apt)
 				echo "Installing dependencies for Charm repo..."
 				sudo mkdir -p /etc/apt/keyrings
-				curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/sources.list.d/charm.list
+				curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+				echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list >/dev/null
 				sudo apt update && sudo apt install gum -y
 				return
 				;;
